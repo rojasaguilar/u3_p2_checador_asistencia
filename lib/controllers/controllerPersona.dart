14147389711ac11b62d3537c0cf340e765f01c4b
Ramses <rojas.devnet@gmail.com>
2025-11-04 15:42:15 -0700

    controller materia y class util bd completadas

---
+++ lib/controllers/controllerPersona.dart
@@ -0,0 +1,26 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:u3_p2_checador_asistencia/models/materia.dart';
+import 'package:u3_p2_checador_asistencia/utils/basedatos.dart';
+
+class ControllerPersona {
+  Future<int> insertarMateria(Materia m) async {
+    Database db = await BD().conectarDB();
+    return db.insert('MATERIA', m.toJSON());
+  }
+
+  Future<List<Materia>> obtenerMaterias() async {
+    Database db = await BD().conectarDB();
+    final List<Map<String, dynamic>> materiasMap = await db.query('MATERIA');
+    return materiasMap.map((materia) => Materia.fromJSON(materia)).toList();
+  }
+
+  Future<Materia> obtenerMateria(String NMAT) async {
+    Database db = await BD().conectarDB();
+
+    final materias = await db.rawQuery('SELECT * FROM MATERIA WHERE NMAT = ?', [
+      NMAT,
+    ]);
+
+    return Materia.fromJSON(materias[0]);
+  }
+}
---
+++ lib/utils/basedatos.dart
@@ -0,0 +1,50 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class BD {
+  Future<Database> conectarDB() async {
+    return openDatabase(
+      join(await getDatabasesPath(), 'checador.db'),
+      version: 1,
+      onConfigure: (db) async => await db.execute('PRAGMA foreign_keys = ON'),
+      onCreate: (db, version) async {
+        //TABLA PROFESOR
+        await db.execute(
+          'CREATE TABLE PROFESOR'
+          '(NPROFESOR TEXT PRIMARY KEY,'
+          ' NOMBRE TEXT,'
+          ' CARRERA TEXT)',
+        );
+
+        await db.execute(
+          'CREATE TABLE MATERIA'
+          '(NMAT TEXT PRIMARY KEY,'
+          ' DESCRIPCION TEXT)',
+        );
+
+        await db.execute(
+          'CREATE TABLE HORARIO'
+          '(NHORARIO INTEGER PRIMARY KEY AUTOINCREMENT,'
+          ' NPROFESOR TEXT,'
+          ' NMAT TEXT,'
+          ' HORA TEXT,'
+          ' EDIFICIO TEXT,'
+          ' SALON TEXT,'
+          ' FOREIGN KEY (NPROFESOR) REFERENCES PROFESOR(NPROFESOR),'
+          ' FOREIGN KEY (NMAT) REFERENCES MATERIA(NMAT)'
+          ' ON DELETE CASCADE ON UPDATE CASCADE)',
+        );
+
+        await db.execute(
+          'CREATE TABLE ASISTENCIA'
+          '(IDASISTENCIA INTEGER PRIMARY KEY AUTOINCREMENT,'
+          ' NHORARIO INT,'
+          ' FECHA TEXT,'
+          ' ASISTENCIA INTEGER DEFAULT 0,'
+          ' FOREIGN KEY (NHORARIO) REFERENCES HORARIO(NHORARIO)'
+          ' ON DELETE CASCADE ON UPDATE CASCADE)',
+        );
+      },
+    );
+  }
+}
