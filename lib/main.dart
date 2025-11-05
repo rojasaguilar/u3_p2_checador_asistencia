dc2c514d895bd719449576ac85ff6d597be0f692
JesusRobles <jeanrobleslo@ittepic.edu.mx>
2025-11-04 21:33:52 -0700

    esqueleto home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:u3_p2_checador_asistencia/pages/addMateria.dart';
+import 'package:u3_p2_checador_asistencia/pages/home.dart';
 import 'package:u3_p2_checador_asistencia/pages/materias.dart';
 
 void main() {
@@ -9,33 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Checador de Asistencia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
---
+++ lib/pages/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _indice = 0;
+
+  final List<Widget> _paginas = [
+    Center(child: Text("Materias", style: TextStyle(fontSize: 24))),
+    Center(child: Text("Horarios", style: TextStyle(fontSize: 24))),
+    Center(child: Text("Maestros", style: TextStyle(fontSize: 24))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Checador de Asistencia"),
+      ),
+      body: _paginas[_indice],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indice,
+        onTap: (index) => setState(() => _indice = index),
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items:  [
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: "Materias"),
+          BottomNavigationBarItem(icon: Icon(Icons.schedule), label: "Horarios"),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: "Maestros"),
+        ],
+      ),
+    );
+  }
+}
